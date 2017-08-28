02c704c40bbc14e02c0f2b4d4396fdc121d8830c
Z-liu <liuzhao@yeah.net>
2017-08-28 16:57:20 +0800

    add

--- leetcode/array.py
+++ leetcode/array.py
@@ -1,4 +1,4 @@
-# Two Sum
+# https://leetcode.com/problems/two-sum/description/
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -27,7 +27,7 @@ class Solution(object):
             nums_dict[num] = i
 
 
-# Remove Duplicates from Sorted Array
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
 class Solution1(object):
     def removeDuplicates(self, nums):
         """
@@ -53,11 +53,45 @@ class Solution1(object):
         return i + 1
 
 
+# https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+class Solution3(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+        def exchange(arr, current_num, current_pos):
+            if arr[current_num - 1] != arr[current_pos]:
+                arr[current_num - 1], arr[current_pos] = arr[current_pos], arr[current_num - 1]
+                current_num = arr[current_pos]
+                exchange(arr, current_num, current_pos)
+            else:
+                return
+
+        n = len(nums)
+        i = 0
+        while i < n:
+            exchange(nums, nums[i], i)
+            i += 1
+        res = []
+        for i in range(len(nums)):
+            if i != nums[i] - 1:
+                res.append(i + 1)
+        return res
+
+
 if __name__ == '__main__':
-    print Solution1().removeDuplicates([0, 0])
-    print Solution1().removeDuplicates([1, 1, 2, 2])
-    print Solution1().removeDuplicates1([0, 0])
-    print Solution1().removeDuplicates1([1, 1, 2, 2])
+    print Solution3().findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])  # 5,6
+    print Solution3().findDisappearedNumbers([1, 3, 2, 7, 8, 2, 3, 1])  # 4,5,6
+    print Solution3().findDisappearedNumbers([1, 3, 2, 7, 8, 5, 5, 1])  # 4,6
+    print Solution3().findDisappearedNumbers([3, 3, 3])
+    print Solution3().findDisappearedNumbers([1, 1, 1])
+    print Solution3().findDisappearedNumbers([2, 2, 2])
+    # print Solution1().removeDuplicates([0, 0])
+    # print Solution1().removeDuplicates([1, 1, 2, 2])
+    # print Solution1().removeDuplicates1([0, 0])
+    # print Solution1().removeDuplicates1([1, 1, 2, 2])
     # print Solution().twoSum([3, 2, 4], 6)
     # print Solution().twoSum1([3, 2, 4], 6)
     # print Solution().twoSum2([3, 2, 4], 6)
