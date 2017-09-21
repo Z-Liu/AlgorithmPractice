a4ef5a7815951d437ead4f02ab3ea67b7b794dce
Z-liu <liuzhao@yeah.net>
2017-09-21 10:15:22 +0800

    add

--- data structures and algorithms/chap7.py
+++ data structures and algorithms/chap7.py
@@ -276,3 +276,74 @@ class PositionList(_DoublyLinkedBase):
         old_value = original._element
         original._element = e
         return old_value
+
+
+def insertion_sort(L):
+    if len(L) > 1:
+        marker = L.first()
+        while marker != L.last():
+            pivot = L.after(marker)
+            value = pivot.element()
+            if value > marker.element():
+                marker = pivot
+            else:
+                walk = marker
+                while walk != L.first() and L.before(walk).element() > value:
+                    walk = L.before(walk)
+                L.delete(pivot)
+                L.add_before(walk, value)
+
+
+class FavoritesList:
+    class _Item:
+        __slots__ = '_value', '_count'
+
+        def __init__(self, e):
+            self._value = e
+            self._count = 0
+
+        def _find_position(self, e):
+            walk = self._data_first()
+            while walk is not None and walk.element()._value != e:
+                walk = self._data.after(walk)
+            return walk
+
+        def _move_up(self, p):
+            if p != self._data.first():
+                cnt = p.element()._count
+                walk = self._data.before(p)
+                if cnt > walk.element()._count:
+                    while (walk != self._data.first() and
+                                   cnt > self._data.before(walk).element()._count):
+                        walk = self._data.before(walk)
+                    self._data.add_before(walk, self._data.delete(p))
+
+        def __init__(self):
+            self._data = PositionList()
+
+        def __len__(self):
+            return len(self._data)
+
+        def is_empty(self):
+            return len(self._data) == 0
+
+        def access(self, e):
+            p = self._find_position(e)
+            if p is None:
+                p = self._data.add_last(self._Item(e))
+            p.element()._count += 1
+            self._move_up(p)
+
+        def remove(self, e):
+            p = self._find_position(e)
+            if p is not None:
+                self._data.delete(p)
+
+        def top(self, k):
+            if not 1 <= k <= len(self):
+                raise ValueError('Illegal value for k')
+            walk = self._data.first()
+            for j in range(k):
+                item = walk.element()
+                yield item._value
+                walk = self._data.after(walk)
