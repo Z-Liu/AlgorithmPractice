888f084fa0dc073a810c19bb983aacb67c100042
Z-liu <liuzhao@yeah.net>
2017-11-03 14:37:01 +0800

    add

---
+++ data structures and algorithms/chap10.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+def word_count(filename):
+    freq = {}
+    for piece in open(filename).read().lower().split():
+        word = ''.join(c for c in piece if c.isalpha())
+        if word:
+            freq[word] = 1 + freq.get(word, 0)
+    max_word = ''
+    max_count = 0
+    for (w, c) in freq.items():
+        if c > max_count:
+            max_word = w
+            max_count = c
+    print('The most frequent word is', max_word)
+    print('Its number of occurrences is', max_count)
