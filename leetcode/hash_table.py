28a22a818a9ea94eea0474ecba0191aeeb7b07a4
Z-liu <liuzhao@yeah.net>
2017-11-21 16:11:51 +0800

    add

--- leetcode/hash_table.py
+++ leetcode/hash_table.py
@@ -33,3 +33,69 @@ class Solution(object):
         for i in nums:
             a ^= i
         return a
+
+
+# https://leetcode.com/problems/valid-anagram/description/
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        dict1 = {}
+        for i in s:
+            if i in dict1:
+                dict1[i] += 1
+            else:
+                dict1[i] = 1
+        for i in t:
+            if i not in dict1:
+                return False
+            dict1[i] -= 1
+        for i in dict1:
+            if dict1[i] != 0:
+                return False
+        return True
+
+    def isAnagram1(self, s, t):
+        if len(s) != len(t):
+            return False
+        return sorted(list(s)) == sorted(list(t))
+
+
+# https://leetcode.com/problems/contains-duplicate/description/
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return bool(len(nums) - len(set(nums)))
+
+
+# https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        dict1 = {}
+        for i in nums1:
+            if i in dict1:
+                dict1[i] += 1
+            else:
+                dict1[i] = 1
+        dict2 = {}
+        for i in nums2:
+            if i in dict2:
+                dict2[i] += 1
+            else:
+                dict2[i] = 1
+        res = []
+        for i in dict1:
+            if i in dict2:
+                res.extend([i] * min(dict1[i], dict2[i]))
+        return res
