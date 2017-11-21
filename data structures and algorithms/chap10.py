0aeffb6bde4c9ec98c04b07bfd72259c7e99ebbe
Z-liu <liuzhao@yeah.net>
2017-11-21 14:56:03 +0800

    add

--- data structures and algorithms/chap10.py
+++ data structures and algorithms/chap10.py
@@ -285,3 +285,38 @@ class CostPerformanceDatabase:
         while other is not None and other[1] <= p:
             del self._M[other[0]]
             other = self._M.find_gt(c)
+
+
+class MultiMap:
+    _MapType = dict
+
+    def __init__(self):
+        self._map = self._MapType()
+        self._n = 0
+
+    def __iter__(self):
+        for k, secondary in self._map.items():
+            for v in secondary:
+                yield k, v
+
+    def add(self, k, v):
+        container = self._map.setdefault(k, [])
+        container.append(v)
+        self._n += 1
+
+    def pop(self, k):
+        secondary = self._map[k]
+        v = secondary.pop()
+        if len(secondary) == 0:
+            del self._map[k]
+        self._n -= 1
+        return k, v
+
+    def find(self, k):
+        secondary = self._map[k]
+        return k, secondary[0]
+
+    def find_all(self, k):
+        secondary = self._map.get(k, [])
+        for v in secondary:
+            yield k, v
