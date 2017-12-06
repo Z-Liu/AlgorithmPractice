9bbe57ec5a6c854bb5952482d8d74c27f49ec941
Z-liu <liuzhao@yeah.net>
2017-12-06 17:03:38 +0800

    add

--- Thinking with Python/chap2.py
+++ Thinking with Python/chap2.py
@@ -44,5 +44,51 @@ class TestProblem4(unittest.TestCase):
         self.assertEqual(len(string_list), 2 ** 10)
 
 
+class Problem6:
+    maxsize = 0
+
+    def __init__(self, cntarr):
+        self.cntarr = cntarr
+
+    def getval(self, A, i, j, L, H):
+        if i < 0 or i >= L or j < 0 or j >= H:
+            return 0
+        else:
+            return A[i][j]
+
+    def findMaxBlock(self, A, r, c, L, H, size):
+        if r >= L or c >= H:
+            return
+        self.cntarr[r][c] = True  # cntarr用于标记某位置在本次测试中是否被测试过
+        size += 1
+        if size > self.maxsize:
+            self.maxsize = size
+        direction = [[-1, 0], [-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1]]
+        for i in range(7):
+            newi = r + direction[i][0]
+            newj = c + direction[i][1]
+            val = self.getval(A, newi, newj, L, H)
+            if val > 0 and self.cntarr[newi][newj] is False:
+                self.findMaxBlock(A, newi, newj, L, H, size)
+        self.cntarr[r][c] = False
+
+    def getMaxOnes(self, A, rmax, colmax):
+        for i in range(rmax):
+            for j in range(colmax):
+                if A[i][j] == 1:
+                    self.findMaxBlock(A, i, j, rmax, colmax, 0)
+        return self.maxsize
+
+
+class TestProblem6(unittest.TestCase):
+    def test_solution(self):
+        zarr = [[1, 1, 0, 0, 0], [0, 1, 1, 0, 1], [0, 0, 0, 1, 1], [1, 0, 0, 1, 1], [0, 1, 0, 1, 1]]
+        rmax = 5
+        colmax = 5
+        problem = Problem6(rmax * [colmax * [False]])
+        res = problem.getMaxOnes(zarr, rmax, colmax)
+        print(res)
+
+
 if __name__ == '__main__':
     unittest.main()
