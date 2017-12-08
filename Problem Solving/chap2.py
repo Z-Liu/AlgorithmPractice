46be34224440ad8beb1e433338eb48775ac8e0a0
Z-liu <liuzhao@yeah.net>
2017-12-08 13:55:46 +0800

    add

--- Problem Solving/chap2.py
+++ Problem Solving/chap2.py
@@ -18,3 +18,39 @@ def anagramSolution1(s1, s2):
             stillOK = False
         pos1 = pos1 + 1
     return stillOK
+
+
+# activecode2
+def anagramSolution2(s1, s2):
+    alist1 = list(s1)
+    alist2 = list(s2)
+    alist1.sort()
+    alist2.sort()
+    pos = 0
+    matches = True
+    while pos < len(s1) and matches:
+        if alist1[pos] == alist2[pos]:
+            pos = pos + 1
+        else:
+            matches = False
+    return matches
+
+
+# activecode3
+def anagramSolution4(s1, s2):
+    c1 = [0] * 26
+    c2 = [0] * 26
+    for i in range(len(s1)):
+        pos = ord(s1[i]) - ord('a')
+        c1[pos] += 1
+    for i in range(len(s2)):
+        pos = ord(s2[i]) - ord('a')
+        c2[pos] += 1
+    j = 0
+    stillOK = True
+    while j < 26 and stillOK:
+        if c1[j] == c2[j]:
+            j += 1
+        else:
+            stillOK = False
+    return stillOK
