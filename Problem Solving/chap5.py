1ac9fdac37c5e0c6f1c2e85144a5054ef07ad52d
Z-liu <liuzhao@yeah.net>
2017-12-13 16:35:27 +0800

    add

--- Problem Solving/chap5.py
+++ Problem Solving/chap5.py
@@ -16,3 +16,41 @@ def shortBubbleSort(alist):
                 exchanges = True
                 alist[i], alist[i + 1] = alist[i + 1], alist[i]
         passnum -= 1
+
+
+def selectionSort(alist):
+    for fillslot in range(len(alist) - 1, 0, -1):
+        positionOfMax = 0
+        for location in range(1, fillslot + 1):
+            if alist[location] > alist[positionOfMax]:
+                positionOfMax = location
+        alist[fillslot], alist[positionOfMax] = alist[positionOfMax], alist[fillslot]
+
+
+def insertionSort(alist):
+    for index in range(1, len(alist)):
+        currentValue = alist[index]
+        position = index
+        while position > 0 and alist[position - 1] > currentValue:
+            alist[position] = alist[position - 1]
+            position -= 1
+        alist[position] = currentValue
+
+
+def shellSort(alist):
+    sublistcount = len(alist) // 2
+    while sublistcount > 0:
+        for startposition in range(sublistcount):
+            gapInsertionSort(alist, startposition, sublistcount)
+        print('After increments of size', sublistcount, 'The list is', alist)
+        sublistcount //= 2
+
+
+def gapInsertionSort(alist, start, gap):
+    for i in range(start + gap, len(alist), gap):
+        currentvalue = alist[i]
+        position = i
+        while position >= gap and alist[position - gap] > currentvalue:
+            alist[position] = alist[position - gap]
+            position -= gap
+        alist[position] = currentvalue
