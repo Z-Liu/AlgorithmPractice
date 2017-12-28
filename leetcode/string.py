46076bdd08d06e77a7b118733bd1ac91120c3a67
Z-liu <liuzhao@yeah.net>
2017-12-28 10:21:36 +0800

    add

--- leetcode/string.py
+++ leetcode/string.py
@@ -29,6 +29,26 @@ class Solution(object):
         return ''.join(i for i in reversed(s))
 
 
+# https://leetcode.com/problems/fizz-buzz/description/
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = [0] * n
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                res[i - 1] = 'FizzBuzz'
+            elif i % 3 == 0:
+                res[i - 1] = 'Fizz'
+            elif i % 5 == 0:
+                res[i - 1] = 'Buzz'
+            else:
+                res[i - 1] = str(i)
+        return res
+
+
 if __name__ == '__main__':
     print(Solution().isValid('()[]{}'))
     print(Solution().isValid('([)]'))
