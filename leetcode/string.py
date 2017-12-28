e42b1abef450c561fd50f8a805743cac894b467d
Z-liu <liuzhao@yeah.net>
2017-12-28 10:12:31 +0800

    add

--- leetcode/string.py
+++ leetcode/string.py
@@ -19,6 +19,16 @@ class Solution(object):
         return len(test) == 0
 
 
+# https://leetcode.com/problems/reverse-string/description/
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return ''.join(i for i in reversed(s))
+
+
 if __name__ == '__main__':
-    print Solution().isValid('()[]{}')
-    print Solution().isValid('([)]')
+    print(Solution().isValid('()[]{}'))
+    print(Solution().isValid('([)]'))
