f93539a4ec6d4c04a45ba24c6a6a1aa5b94dc059
Z-liu <liuzhao@yeah.net>
2017-12-28 17:34:59 +0800

    add

--- leetcode/hash_table.py
+++ leetcode/hash_table.py
@@ -164,3 +164,35 @@ class Solution(object):
                     j += 1
             i += 1
         return count
+
+
+# https://leetcode.com/problems/roman-to-integer/description/
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        amap = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        alist = []
+        for i in s:
+            alist.append(amap[i])
+        res = 0
+        i = len(alist) - 1
+        maxnum = 0
+        while i >= 0:
+            if alist[i] >= maxnum:
+                res += alist[i]
+                maxnum = alist[i]
+            else:
+                res -= alist[i]
+            i -= 1
+        return res
